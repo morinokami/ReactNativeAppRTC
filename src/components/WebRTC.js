6c91f3bee4b1794438760f13a8a13ade15e7c5b9
Shinya Fujino <shf0811@gmail.com>
2020-08-29 12:58:04 +0900

    Install react-native-webrtc and configure Android settings

--- src/components/WebRTC.js
+++ src/components/WebRTC.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import {Text} from 'react-native';
+import {mediaDevices} from 'react-native-webrtc';
 
 const WebRTC = ({route}) => {
   console.log(route.params);
+  mediaDevices.enumerateDevices().then((sourceInfos) => {
+    console.log(sourceInfos);
+  });
   return <Text>Hey</Text>;
 };
 
