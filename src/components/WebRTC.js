e2cb279dc3840efbd3a93cf42e3812b1abd0fb7c
Shinya Fujino <shf0811@gmail.com>
2020-08-30 16:36:34 +0900

    Implement cliand and server

--- src/components/WebRTC.js
+++ src/components/WebRTC.js
@@ -1,48 +1,213 @@
 import React, {useState, useEffect} from 'react';
 import {StyleSheet, View, Dimensions} from 'react-native';
-import {RTCView, mediaDevices} from 'react-native-webrtc';
+import {
+  RTCView,
+  mediaDevices,
+  RTCPeerConnection,
+  RTCSessionDescription,
+  RTCIceCandidate,
+} from 'react-native-webrtc';
+import WebSocketClient from 'reconnecting-websocket';
 
 const dimensions = Dimensions.get('window');
 
-const WebRTC = ({route}) => {
-  const [localStreamURL, setLocalStreamURL] = useState(null);
-  const [remoteStreamURL, setRemoteStreamURL] = useState(null);
+const host = '192.168.1.19:8765';
+const id = '_' + Math.random().toString(36).substr(2, 9);
+let remoteId = null;
+let connection = null;
 
-  console.log(route.params);
-  mediaDevices.enumerateDevices().then((sourceInfos) => {
-    console.log(sourceInfos);
-  });
+const WebRTC = ({route, navigation}) => {
+  const room = route.params.room;
+  const [localStream, setLocalStream] = useState(null);
+  const [remoteStream, setRemoteStream] = useState(null);
+
   useEffect(() => {
-    mediaDevices
-      .getUserMedia({
-        video: {
-          mandatory: {
-            minWidth: 500,
-            minHeight: 300,
-            minFrameRate: 30,
+    const ws = new WebSocketClient(`ws://${host}`);
+
+    ws.onopen = () => {
+      console.log('Connected');
+    };
+
+    ws.onerror = (err) => {
+      console.log('Got error:', err);
+    };
+
+    ws.onmessage = async (message) => {
+      console.log('Got message:', message);
+      const data = JSON.parse(message.data);
+      switch (data.type) {
+        case 'enter':
+          await onEnter(data.success, data.full, data.wait);
+          break;
+        case 'offer':
+          await onOffer(data.offer, data.remoteId);
+          break;
+        case 'answer':
+          await onAnswer(data.answer);
+          break;
+        case 'candidate':
+          await onCandidate(data.candidate);
+          break;
+        case 'leave':
+          await onLeave();
+          break;
+        default:
+          break;
+      }
+    };
+
+    const send = (message) => {
+      message.room = room;
+      if (remoteId) {
+        message.remoteId = remoteId;
+      }
+      message.id = id;
+      ws.send(JSON.stringify(message));
+    };
+
+    const onEnter = async (success, full, wait) => {
+      if (success) {
+        if (wait) {
+          startConnection();
+        } else {
+          startConnection(true);
+        }
+      } else {
+        if (full) {
+          //TODO: full
+        } else {
+          //TODO:
+        }
+      }
+    };
+
+    const startConnection = (startPC = false) => {
+      mediaDevices
+        .getUserMedia({
+          video: {
+            mandatory: {
+              minWidth: 500,
+              minHeight: 300,
+              minFrameRate: 30,
+            },
           },
-        },
-      })
-      .then((stream) => {
-        setLocalStreamURL(stream.toURL());
-      })
-      .catch((err) => console.error(err));
-  }, []);
+        })
+        .then(async (stream) => {
+          setLocalStream(stream);
+          await setupPeerConnection(stream, startPC);
+        })
+        .catch((err) => console.error(err));
+    };
+
+    const setupPeerConnection = async (stream, startPC = false) => {
+      const configuration = {
+        iceServers: [
+          {
+            urls: 'stun:stun.l.google.com:19302',
+          },
+        ],
+      };
+      const conn = new RTCPeerConnection(configuration);
+      connection = conn;
+
+      conn.addStream(stream);
+      conn.onaddstream = (event) => setRemoteStream(event.stream);
+
+      conn.onicecandidate = (event) => {
+        if (event.candidate) {
+          send({
+            type: 'candidate',
+            candidate: event.candidate,
+          });
+        }
+      };
+
+      if (startPC) {
+        await startPeerConnection();
+      }
+    };
+
+    const startPeerConnection = async () => {
+      try {
+        const offer = await connection.createOffer();
+        send({
+          type: 'offer',
+          offer,
+        });
+        await connection.setLocalDescription(offer);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    const onOffer = async (offer, remote) => {
+      remoteId = remote;
+      await connection.setRemoteDescription(new RTCSessionDescription(offer));
+
+      try {
+        const answer = await connection.createAnswer();
+        await connection.setLocalDescription(answer);
+        send({
+          type: 'answer',
+          answer,
+        });
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    const onAnswer = async (answer) =>
+      await connection.setRemoteDescription(new RTCSessionDescription(answer));
+
+    const onCandidate = async (candidate) =>
+      await connection.addIceCandidate(new RTCIceCandidate(candidate));
+
+    const onLeave = async () => {
+      remoteId = null;
+      setRemoteStream(null);
+      connection.close();
+      connection.onaddstream = null;
+      connection.onicecandidate = null;
+      startConnection();
+    };
+
+    if (room.length > 0) {
+      send({
+        type: 'enter',
+        id,
+      });
+    } else {
+      // Error
+    }
+
+    const unsubscribe = navigation.addListener('blur', () => {
+      connection.close();
+      connection.onaddstream = null;
+      connection.onicecandidate = null;
+      send({
+        type: 'leave',
+      });
+    });
+    return unsubscribe;
+  }, [navigation, room]);
 
   return (
     <View style={styles.container}>
       <View style={styles.video}>
         <View style={styles.localVideo}>
           <View style={styles.videoWidget}>
-            {localStreamURL && (
-              <RTCView streamURL={localStreamURL} style={styles.rtcView} />
+            {localStream && (
+              <RTCView streamURL={localStream.toURL()} style={styles.rtcView} />
             )}
           </View>
         </View>
         <View style={styles.remoteVideo}>
           <View style={styles.videoWidget}>
-            {remoteStreamURL && (
-              <RTCView streamURL={remoteStreamURL} style={styles.rtcView} />
+            {remoteStream && (
+              <RTCView
+                streamURL={remoteStream.toURL()}
+                style={styles.rtcView}
+              />
             )}
           </View>
         </View>
