9bf89c3b4063baaa6dfbc0c9f7e1a728e10b631f
Shinya Fujino <shf0811@gmail.com>
2020-08-30 16:54:05 +0900

    Update styles of Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,22 +1,23 @@
 import React from 'react';
-import {StyleSheet, View, Text, TextInput, Button, Alert} from 'react-native';
+import {StyleSheet, View, Text, TextInput, Button} from 'react-native';
 
 const Home = ({navigation}) => {
   const [room, setRoom] = React.useState('1234');
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>AppRTC</Text>
-      <Text>Please enter a room name.</Text>
-      <TextInput onChangeText={(text) => setRoom(text)} value={room} />
+      <Text style={styles.title}>AppRTC Clone</Text>
+      <Text style={styles.instruction}>Please enter a room name.</Text>
+      <TextInput
+        onChangeText={(text) => setRoom(text)}
+        value={room}
+        style={styles.roomInput}
+        keyboardType="number-pad"
+      />
       <View style={styles.buttons}>
         <Button
           title="Join"
           onPress={() => navigation.navigate('WebRTC', {room})}
-        />
-        <Button
-          title="Random"
-          onPress={() => Alert.alert('Random button pressed')}
         />
       </View>
     </View>
@@ -32,9 +33,16 @@ const styles = StyleSheet.create({
   },
   title: {
     color: '#fff',
+    fontSize: 20,
+  },
+  instruction: {
+    color: '#eee',
   },
   roomInput: {
+    color: '#fff',
     height: 40,
+    borderBottomColor: '#4c4c4f',
+    borderBottomWidth: 1,
   },
   buttons: {
     flexDirection: 'row',
